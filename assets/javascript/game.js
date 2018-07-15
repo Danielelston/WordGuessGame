dca71bd615320b7c2c2abe294a7c0200b2c8f20b
Daniel.ELston <Danielelston94@gmail.com>
2018-07-14 21:57:07 -0400

    create most game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,11 @@ var win = 0;
 var lose = 0;
 var remainingGuesses = 10;
 var userQuesses = [];
+var blankWord = [];
+var blankWordUF
 
 var wordList = ["lightning", "squall", "sephiroth", "tidus", "tifa", "cloud", "noctis", "yuffie"];
-
+var wordArray = [];
 
 
 // Get random word
@@ -12,21 +14,29 @@ function gameStart() {
 
     var rng = [Math.floor(Math.random() * wordList.length)];
     var word = wordList[rng];
-    var wordLength = word.length;
 
     console.log(word);
     alert(word);
 
-    var blankWord = "";
+    blankWordUF = (blankWord.join(" ")); 
 
-    for (var i = 0; i < wordLength; i++) {
-        
-        blankWord = blankWord + "_";
-        
+
+
+    for (var i = 0; i < word.length; i++) {
+
+        blankWord.push("_");
+
     }
     console.log(blankWord);
-    document.getElementById("gameBlanks").innerHTML = blankWord;
+    console.log(blankWordUF);
+    updateBlanks();
+    // document.getElementById("gameBlanks").innerHTML = (blankWord.join(" "));
 
+    for (var a = 0; a < word.length; a++) {
+        var aChar = word.charAt(a);
+        wordArray.push(aChar);
+    }
+    console.log(wordArray);
 
     var startScreen = document.getElementById("startScreenId");
     startScreen.style.display = "none";
@@ -42,11 +52,18 @@ function gameStart() {
 
 };
 
-// document.onkeyup = function userInput(event){
+var updateBlanks = function (){
+    document.getElementById("gameBlanks").innerHTML = (blankWord.join(" "));
 
-// }
+};
+
+var updateGuessCounter = function (){
+    document.getElementById("guesses").innerHTML = "Remaining guesses: " + remainingGuesses;
+};
 
 function wordGame() {
+
+   
 
     console.log("wordgame started")
     // document.body.addEventListener("keyup", logic);
@@ -54,30 +71,44 @@ function wordGame() {
         var userKey = event.key;
         console.log(userKey);
 
-
         if (userQuesses.indexOf(userKey) === -1) {
             userQuesses.push(userKey);
-        } else {
-            remainingGuesses--;
-            document.getElementById("guesses").innerHTML = "Remaining guesses: " + remainingGuesses;
+            updateGuessCounter();
+            // document.getElementById("userGuessedLetters").innerHTML = "Guesses:" + userQuesses;
+            // document.getElementById("guesses").innerHTML = "Remaining guesses: " + remainingGuesses;
+            if (wordArray.indexOf(userKey) > -1) {
 
+                // logic for filling in correct letter guess-------------------------------------------------
+                alert("correct guess!");
+            } else {
+                remainingGuesses--;
+                updateGuessCounter();
+                //  document.getElementById("guesses").innerHTML = "Remaining guesses: " + remainingGuesses;
+
+                if (remainingGuesses < 1) {
+                    alert("you died");
+                    // reset vars and send user to new game screen------------------------------------
+                }
+            }
+        } else {
+            // document.getElementById("guesses").innerHTML = "Remaining guesses: " + remainingGuesses;
+            alert("repeat quess!");
         }
-        
+
         for (var A = 0; A < userQuesses.length; A++) {
-            
-            
+
+
         }
         // if(userKey)
         console.log(userQuesses);
 
-        
+
     };
     // var userInput = 
 }
 
-function logic(){
+function logic() {
 
 }
 
 
-    
